dc6dc091b157b1e3ac90a3d89a7b69ff6ce5a4c5
dmdraghici@gmail.com <dmdraghici@gmail.com>
2021-09-29 17:10:10 +0300

    commit29.09.2021

--- src/startup/schema.js
+++ src/startup/schema.js
@@ -1,18 +1,19 @@
 const { makeExecutableSchema } = require('apollo-server-koa')
 const merge = require('lodash.merge')
-const helloWorldResolvers = require('../features/helloWorld/resolvers');
-
+const helloWorldResolvers = require('../features/helloWorld/resolvers')
 const { loadTypedefsSync } = require('@graphql-tools/load')
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader')
 const { join } = require('path')
 
 const userResolvers = require('../features/user/resolvers')
+const conferenceResolvers = require('../features/conference/resolvers')
+const dictionaryResolvers = require('../features/dictionaries/resolvers')
 
 const oldTypeDefs = []
 const sources = loadTypedefsSync(join(__dirname, '../**/*.graphql'), {
   loaders: [new GraphQLFileLoader()]
 })
-const resolvers = merge(userResolvers, helloWorldResolvers)
+const resolvers = merge(userResolvers, helloWorldResolvers, conferenceResolvers)
 
 const typeDefs = [...sources.map(source => source.document), ...oldTypeDefs]
 
